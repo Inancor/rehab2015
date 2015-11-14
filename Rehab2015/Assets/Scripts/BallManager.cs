3d8728bf58e89e0099bd1c0c6b2a8f2d723e8e4c
Simon Stannus <Simon.Stannus@gmail.com>
2015-11-14 17:12:56 +1100

    Added Ball generation and physics

--- Rehab2015/Assets/Scripts/BallManager.cs
+++ Rehab2015/Assets/Scripts/BallManager.cs
@@ -7,9 +7,34 @@ public class BallManager : MonoBehaviour {
 	void Start () {
 	
 	}
-	
+
+	public Ball spawnObject;
+	public Vector3 spawnCentre;
+	public float spawnRadius;
+	public Vector3 startingVelocity;
+	public float ballDelay;
+
+	private float timeUntilNextBall = 0;
+
+
 	// Update is called once per frame
 	void Update () {
-	
+
+		timeUntilNextBall -= Time.deltaTime;
+
+		if (timeUntilNextBall < 0)
+		{
+			timeUntilNextBall = ballDelay;
+
+			float x = spawnCentre.x + 2 * (Random.value - 0.5f) * spawnRadius;
+			float y = spawnCentre.y + 2 * (Random.value - 0.5f) * spawnRadius;
+			float z = spawnCentre.z + 2 * (Random.value - 0.5f) * spawnRadius;
+			Vector3 spawnPosition = new Vector3(x, y, z);
+			//Instantiate(spawnObject, spawnPosition, Quaternion.identity);
+
+			Ball ball = Instantiate<Ball>(spawnObject);
+			ball.transform.position = spawnPosition;
+			ball.setVelocity(startingVelocity);
+		}
 	}
 }
