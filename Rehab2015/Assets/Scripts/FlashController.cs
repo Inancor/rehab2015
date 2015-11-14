c47f474add3d13497b88ac59cf2a6513f44c021c
oakla <3d.lanz@gmail.com>
2015-11-15 00:50:25 +1100

    Added particles/explosion and their materials

---
+++ Rehab2015/Assets/Scripts/FlashController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        explode();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void explode ()
+    {
+        var exp = GetComponent<ParticleSystem>();
+        exp.Play();
+        Destroy(gameObject, exp.duration);
+    }
+}
---
+++ Rehab2015/Assets/testBallScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testBallScript : MonoBehaviour {
+
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal =
+    }
+}
