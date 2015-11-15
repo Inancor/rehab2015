b1a993ece51fc324f2378495ef305ac95d6a7d3b
Simon Stannus <Simon.Stannus@gmail.com>
2015-11-15 16:18:19 +1100

    Added main menu scene, fixed scene list in build settings and completed key-effected scene transitions.

---
+++ Rehab2015/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public string LevelName;
+
+    // Use this for initialization
+	void Start () {
+	}
+	
+    public void ChangeLevel()
+    {
+        Application.LoadLevel(LevelName);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Rehab2015/Assets/Scripts/MenuSelect.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSelect : MonoBehaviour {
+    RaycastHit hit;
+    TextMesh text;
+    Color defaultColor;
+    public string ClickEvent;
+    bool hovered = false;
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Application.LoadLevel("SixenseHands");
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.tag == "MenuButton" && hit.collider.isTrigger)
+            {
+                text = hit.collider.GetComponent<TextMesh>();
+                if (!hovered) defaultColor = text.color;
+                text.color = new Color(1, 0, 0);
+                hovered = true;
+            }
+            else if(hit.collider.tag == "PlayGameButton")
+            {
+                text = hit.collider.GetComponent<TextMesh>();
+                if (!hovered) defaultColor = text.color;
+                text.color = new Color(1, 0, 0);
+                hovered = true;
+
+            }
+
+            else
+            {
+                if (text != null)
+                {
+                    if (hovered)
+                    {
+                        text.color = defaultColor;
+                    }
+                }
+            }
+        }
+        else
+        {
+
+            if(text != null)
+            {
+                if(hovered)
+                {
+                    text.color = defaultColor;
+                }
+            }
+
+        }
+    }
+}
